63113e44dc76ce318c973847e61989833239beba
ksharlen <miily@list.ru>
2019-04-12 12:43:40 +0300

    12:43

---
+++ 42/C_ansi/strings/includes/ft_sym_header.h
@@ -0,0 +1,14 @@
+#ifndef FT_SYM_HEADER_H
+# define FT_SYM_HEADER_H
+
+# include "ft_str_header.h"
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+#endif
