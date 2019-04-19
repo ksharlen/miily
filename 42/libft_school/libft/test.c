580a30b65f00b37f76c41b390f626a38d95a5ef0
Kharazim Sharlene <ksharlen@mi-t3.21-school.ru>
2019-04-19 12:53:29 +0300

    12:53

--- 42/libft_school/libft/test.c
+++ 42/libft_school/libft/test.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include "libft.h"
+#include <string.h>
 
 int		main(void)
 {
-	printf("%d", -1123 / 1000);
+	char *data = "hello, i'm a data";
+	t_list *l = ft_lstnew(NULL, 5);
+	int temp;
 
+	printf("data = %s\n", data);
+	printf("l = %s\n", l->content);
+	temp = ft_strcmp(data, l->content);
+	printf("cmp = %d\n", temp);
+
+/*	if (!strcmp(data, l->content))
+	{
+		free(l->content);
+		free(l);
+		exit(TEST_SUCCESS);
+	}
+*/
 	return (0);
 }
