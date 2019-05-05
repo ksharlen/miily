2224011063ca7a55f3894f1f1f8f2f5d1b671b6d
ksharlen <miily@list.ru>
2019-05-05 18:16:26 +0300

    18:16

--- training/temp/test.c
+++ training/temp/test.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "libft.h"
 
-int main(void)
+int     main(void)
 {
-    int a;
-    a = 0;
+    size_t len;
+    char s[] = "hello world";
 
-    printf("%d", a);
+    len = ft_strnlen(s, 'o');
+    // printf("char = %lu\n", sizeof(char));
+    // printf("short = %lu\n", sizeof(short));
+    // printf("int = %lu\n", sizeof(int));
+    // printf("long = %lu\n", sizeof(long));
+    // printf("float = %lu\n", sizeof(float));
+    // printf("double = %lu\n", sizeof(double));
+    // printf("signed = %lu\n", sizeof(signed));
+    // printf("unsigned = %lu\n", sizeof(unsigned));
+    // printf("long long = %lu\n", sizeof(long long));
+    // printf("long double = %lu\n", sizeof(long double));
+    // printf("long int = %lu\n", sizeof(long int));
     return (0);
 }
