8e23cde08bf0320a10ef406e2ea9bdf014425030
ksharlen <miily@list.ru>
2019-05-06 01:49:41 +0300

    1:49

--- training/temp/test.c
+++ training/temp/test.c
@@ -5,20 +5,6 @@
 
 int     main(void)
 {
-    size_t len;
-    char s[] = "hello world";
 
-    len = ft_strnlen(s, 'o');
-    // printf("char = %lu\n", sizeof(char));
-    // printf("short = %lu\n", sizeof(short));
-    // printf("int = %lu\n", sizeof(int));
-    // printf("long = %lu\n", sizeof(long));
-    // printf("float = %lu\n", sizeof(float));
-    // printf("double = %lu\n", sizeof(double));
-    // printf("signed = %lu\n", sizeof(signed));
-    // printf("unsigned = %lu\n", sizeof(unsigned));
-    // printf("long long = %lu\n", sizeof(long long));
-    // printf("long double = %lu\n", sizeof(long double));
-    // printf("long int = %lu\n", sizeof(long int));
     return (0);
 }
