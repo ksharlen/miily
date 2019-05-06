e9cbdcda2671634be0b16db8c9dadb98a47faa00
ksharlen <miily@list.ru>
2019-05-06 22:07:39 +0300

    22:07

--- training/temp/main.c
+++ training/temp/main.c
@@ -2,17 +2,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdlib.h>
 
 int     main(void)
 {
-    int fd;
-    char buf[50];
-    int byte_read;
+    void *test;
+    char *p = "hello world";
 
-    fd = open("test", O_RDONLY);
-    printf("fd = %d\n", fd);
-    byte_read = read(fd, buf, 50);
-    //buf[byte_read] = 0;
-    printf("buf = %s\n", buf);
+    test = malloc((sizeof(char) * strlen(p) + 1) + sizeof(int));
+    (*(int *)test) = 24;
+    memcpy(test + 4, p, strlen(p) + 1);
+    printf("testInt = %d\ntestStr = %s\n", (*(int *)test), (char *)test + 4);
     return (0);
 }
