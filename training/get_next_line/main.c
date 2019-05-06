8730e0e662d74189344b1e0244340a7e29250ba6
ksharlen_note <miilytor32@gmail.com>
2019-05-06 12:38:39 +0300

    12:38

--- training/get_next_line/main.c
+++ training/get_next_line/main.c
@@ -2,10 +2,14 @@
 
 int     main(void)
 {
-    char *p;
+    t_list *beg;
+    t_list *find;
 
-    p = NULL;
-    get_next_line(13, &p);
-    get_next_line(24, &p);
+    beg = NULL;
+    beg = ft_lstnew("hello world", ft_strlen("hello world") + 1);
+    // get_next_line(13, &beg);
+    // get_next_line(24, &beg);
+    find = ft_lstelem(&beg, 0);
+    printf("find->content = %s\n", find->content);
     return (0);
 }
