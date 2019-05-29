fe0e519081cf7ff500fc85aa8f62ab3e94ea816a
ksharlen <miily@list.ru>
2019-05-30 00:31:32 +0300

    0:31

---
+++ C_training/ft_printf/ft_printf.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/29 07:55:50 by marvin            #+#    #+#             */
+/*   Updated: 2019/05/29 07:55:50 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+
+typedef union 			test
+{
+	long long			ll;
+	unsigned long long	llu;
+	long				l;
+	unsigned long		lu;
+	int					d;
+	unsigned int		u;
+	short 				hd;
+	unsigned short		hhd;
+//	char				c;
+	unsigned char 		c;
+}						type;
+
+int		ft_printf(const char *format, ...);
+
+#endif
