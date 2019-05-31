8e82f491e458be9286075b42eb7048fa5202be33
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-05-31 21:43:24 +0300

    21:43

--- C_training/ft_printf/ft_printf.c
+++ C_training/ft_printf/ft_printf.c
@@ -3,19 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:41 by marvin            #+#    #+#             */
-/*   Updated: 2019/05/29 07:55:41 by marvin           ###   ########.fr       */
+/*   Updated: 2019/05/31 21:42:34 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static long		ft_type_to_long(va_list form, char type)
+{
+	if (type == 'd')
+		return (va_arg(form, int));
+	return (0);
+}
+
 int		ft_printf(const char *format, ...)
 {
-	va_list var_over;
+	va_list form;
+	long	var;
 
-	while (st)
+	va_start(form, format);
+	
 	return (0);
 }
--- C_training/ft_printf/temp/main.c
+++ C_training/ft_printf/temp/main.c
@@ -5,15 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-/*   Updated: 2019/05/31 17:41:02 by ksharlen         ###   ########.fr       */
+/*   Created: 2019/05/31 17:43:14 by ksharlen          #+#    #+#             */
+/*   Updated: 2019/05/31 17:45:45 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../libft/libft.h"
+#include <stdarg.h>
+
+#define INT 	int
+#define CHAR 	char
+
+int		test(char type, int num, ...)
+{
+	va_list form;
+
+	va_start(form, num);
+	type == 'd' ? INT : CHAR;
+}
 
 int		main(void)
 {
-	printf("hello %d", 24);
-	return (0);
+	test()
 }
