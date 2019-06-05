d84e168a2ef297fb5111f968cc24c492041ade9c
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-06-05 17:05:23 +0300

    17:05

--- C_training/ft_printf/main_ft_printf.c
+++ C_training/ft_printf/main_ft_printf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_ft_printf.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ksharlen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-/*   Updated: 2019/06/04 11:23:52 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/05 16:46:26 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int		main(void)
 {
-	ft_printf("hello %20.100lld", 24);
+	ft_printf("%d");
+	ft_printf("%ld");
+	ft_printf("%lld");
+	ft_printf("%f");
+	ft_printf("%lf");
+	ft_printf("%p");
+	ft_printf("%lp");
 	return (0);
 }
--- C_training/ft_printf/src/src_control/ft_work_to_format.c
+++ C_training/ft_printf/src/src_control/ft_work_to_format.c
@@ -3,28 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_work_to_format.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ksharlen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 11:42:37 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/04 11:24:28 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/05 17:01:02 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static char			*ft_detect_format_size(const char *format)
+static char			*ft_detect_format_size(const char *format, size_t *shift_format)
 {
-	if (*format == 'l')
+	if ((*format == 'l') && ((*(format + 1)) != 'p') && ((*(format + 1) != 'n')))
 	{
 		if (*(++format) == 'l')
 			if (ft_strchr(TYPE, *(++format)))
+			{
+				*shift_format = 2;
 				return (ft_memnjoin("ll", format, 2, 1));
+			}
+		return (ft_memnjoin("l", format, 1, 1));
 	}
 	else if (*format == 'h')
 	{
 		if (*(++format) == 'h')
 			if (ft_strchr(TYPE, *format))
 				return (ft_memnjoin("hh", format, 2, 1));
+		return (ft_memnjoin("h", format, 1, 1));
 	}
 	else
 	{
@@ -39,7 +44,9 @@ static char			*ft_check_format(const char *format)
 	size_t			num_before_format;
 	char			*ret;
 	char			*test_format;
+	size_t			shift_format;
 
+	shift_format = 0;
 	ret = NULL;
 	num_before_format = 0;
 	while (!(ft_isalpha(*format)) && (*format))
@@ -47,9 +54,9 @@ static char			*ft_check_format(const char *format)
 		num_before_format++;
 		format++;
 	}
-	test_format = ft_detect_format_size(format);
-	printf("test_format = %s", test_format);
-	exit(1);
+	test_format = ft_detect_format_size(format, &shift_format);
+	printf("test_format = %s\nnum_before_format = %ld\n", test_format, num_before_format);
+	//exit(1);
 	return (ret);
 }
 
@@ -66,7 +73,7 @@ void			ft_work_to_format(const char *format, char *buf_printf, va_list form)
 		if (*format == '%')
 		{
 			p_format = ft_check_format(format + 1);
-			
+			//ft_control_spec()
 		}
 		else
 			buf_printf[i] = *format;
