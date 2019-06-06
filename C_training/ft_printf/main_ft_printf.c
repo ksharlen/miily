8ac3526caf298d1abf3f6f1c19a8c38aee7b5efa
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-06-06 15:22:33 +0300

    15:22

--- C_training/ft_printf/main_ft_printf.c
+++ C_training/ft_printf/main_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-/*   Updated: 2019/06/05 16:46:26 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/06 10:48:42 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int		main(void)
 {
-	ft_printf("%d");
-	ft_printf("%ld");
-	ft_printf("%lld");
-	ft_printf("%f");
-	ft_printf("%lf");
-	ft_printf("%p");
-	ft_printf("%lp");
+	//ft_printf("%d");
+	ft_printf("%hf");
+	// ft_printf("%f");
+	// ft_printf("%lf");
+	// ft_printf("%p");
+	// ft_printf("%llp");
+	// ft_printf("%lln");
 	return (0);
 }
