2c26690207a9e73c87a3b7069d707a2fce127c12
ildison <ildarusmanov92@gmail.com>
2019-06-07 10:30:32 +0300

    10.30

--- C_training/ft_printf/src/src_num_to_str/main.c
+++ C_training/ft_printf/src/src_num_to_str/main.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_num2binary.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/07 09:21:52 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/07 10:00:14 by cormund          ###   ########.fr       */
+/*   Created: 2019/06/07 09:25:50 by cormund           #+#    #+#             */
+/*   Updated: 2019/06/07 09:47:08 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char		*ft_num2binary(long int num)
-{
-	char	tmp_bin[31];
-	char	*bin;
-	char	*start_bin;
-	int		len;
 
-	len = 0;
-	*tmp_bin = '0';
-	while (num>>len)
-		tmp_bin[len++] = (num & 1 ? '1' : '0');
-	len ? 1 : len++;
-	bin = (char *)malloc((len + 1) * sizeof(char));
-	if (!(start_bin = bin))
-		return (NULL);
-	bin[len] = '\0';
-	while (len--)
-		*bin++ = tmp_bin[len];
-	return (start_bin);
+int	main(int ac, char **av)
+{
+	ft_putstr(ft_num2binary(ft_atoi(av[1])));
+	return (0);
 }
