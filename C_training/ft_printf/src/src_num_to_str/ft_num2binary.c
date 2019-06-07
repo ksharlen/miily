0400a39b6618f42917a565d11d7e55f50eef48e3
Chugtai Ormund <cormund@mi-t4.21-school.ru>
2019-06-07 17:13:27 +0300

    17:13

--- C_training/ft_printf/src/src_num_to_str/ft_num2binary.c
+++ C_training/ft_printf/src/src_num_to_str/ft_num2binary.c
@@ -6,27 +6,34 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 09:21:52 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/07 13:50:21 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/07 17:09:56 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char		*ft_num2binary(long int num)
+static size_t	ft_bit_depth(long int num)
 {
-	char	*bin;
-	char	*start_bin;
-	size_t	len;
+	size_t		len;
 
 	len = 0;
-	while (num>>len && len != 32)
+	while (num >> len && len != 64)
 		++len;
-	len ? 1 : len++;
+	return (len ? len : ++len);
+}
+
+char			*ft_num2binary(long int num)
+{
+	char		*bin;
+	char		*start_bin;
+	size_t		len;
+
+	len = ft_bit_depth(num);
 	bin = (char *)malloc((len + 1) * sizeof(char));
 	if (!(start_bin = bin))
 		return (NULL);
 	bin[len] = '\0';
 	while (len--)
-		*bin++ = (num>>len & 1) + '0';
+		*bin++ = (num >> len & 1) + '0';
 	return (start_bin);
 }
