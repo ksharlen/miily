b0a2af70a9ba18b30e460357b7a147b9deaf4cc7
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-06-07 09:20:09 +0300

    09:20

--- C_training/ft_printf/ft_printf.h
+++ C_training/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ksharlen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 12:15:04 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/06 17:21:17 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/07 09:13:09 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,19 @@
 # define DEC	4
 # define SPACE	8
 # define HASH	16
-# define DOES_NOT_SUPPORT_LONG "nprkfeEgGaAD"
+# define DOES_NOT_SUPPORT_LONG "nprkeEgGaAD"
 # define DOES_NOT_SUPPORT_SHORT "feEgGaAnprkD"
-# define DOES_NOT_SUPPORT_INT_MAX "oOxXufeEgGaAnprkUD"
-# define DOSE_NOT_SUPPORT_LONG_FLOAT "dioOxXunprkDU"
+# define DOES_NOT_SUPPORT_INT_MAX "feEgGaAnprkUDxXoOuU"
+# define DOES_NOT_SUPPORT_LONG_FLOAT "dioOxXunprkDU"
+# define DOES_NOT_SUPPORT_SIZE_T "feEgG"
+# define DOES_NOT_SUPPORT_L "dioOxXunprkDU"
 
 typedef struct			s_spec
 {
 	short				flags;
 	int					width;
 	int					accuracy;
+	size_t				ret_printf;
 }						t_spec;
 
 extern t_spec			g_spec;
@@ -40,9 +43,10 @@ int			ft_printf(const char *format, ...);
 void		ft_error_find(const char *str, int line);
 void		ft_work_to_format(const char *format, char *buf_printf, va_list form);
 long		ft_control_format_int(va_list form, char *type);
-int			ft_check_size_format(const char symbal_irr);
 char		*ft_l_format(const char *format);
 char		*ft_h_format(const char *format);
 char		*ft_j_format(const char *format);
+char		*ft_z_format(const char *format);
+char		*ft_l_big_format(const char *format);
 
 #endif
