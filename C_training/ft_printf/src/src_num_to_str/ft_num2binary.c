436f0ec1cd524a8f7b482a38ea598abafe0c36c7
ildison <ildarusmanov92@gmail.com>
2019-06-07 13:50:41 +0300

    13:50

--- C_training/ft_printf/src/src_num_to_str/ft_num2binary.c
+++ C_training/ft_printf/src/src_num_to_str/ft_num2binary.c
@@ -6,7 +6,7 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 09:21:52 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/07 10:00:14 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/07 13:50:21 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 char		*ft_num2binary(long int num)
 {
-	char	tmp_bin[31];
 	char	*bin;
 	char	*start_bin;
-	int		len;
+	size_t	len;
 
 	len = 0;
-	*tmp_bin = '0';
-	while (num>>len)
-		tmp_bin[len++] = (num & 1 ? '1' : '0');
+	while (num>>len && len != 32)
+		++len;
 	len ? 1 : len++;
 	bin = (char *)malloc((len + 1) * sizeof(char));
 	if (!(start_bin = bin))
 		return (NULL);
 	bin[len] = '\0';
 	while (len--)
-		*bin++ = tmp_bin[len];
+		*bin++ = (num>>len & 1) + '0';
 	return (start_bin);
 }
