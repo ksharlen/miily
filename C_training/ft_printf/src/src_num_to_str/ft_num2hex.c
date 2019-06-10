ed8f1deb6a166d2cdc7e349e471e4b54c715cb22
ildison <ildarusmanov92@gmail.com>
2019-06-10 12:45:09 +0300

    12:45

--- C_training/ft_printf/src/src_num_to_str/ft_num2hex.c
+++ C_training/ft_printf/src/src_num_to_str/ft_num2hex.c
@@ -6,13 +6,22 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 17:11:44 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/07 17:12:37 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/07 17:19:40 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char    *ft_num2hex(long int num)
+char		*ft_num2hex(long int num)
 {
-    
+	char	*hex;
+	size_t	len;
+
+	len = 0;
+	while (num/16)
+	{
+		++len;
+	}
+	printf("len = %zu\n", len);
+	return (NULL);
 }
