34598e5eccac98b3719ee667b477ee1de50cc2e8
ildison <ildarusmanov92@gmail.com>
2019-06-10 15:42:28 +0300

    15:42

--- C_training/ft_printf/src/src_num_to_str/ft_num2hex_or_oct.c
+++ C_training/ft_printf/src/src_num_to_str/ft_num2hex_or_oct.c
@@ -6,20 +6,37 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 17:11:44 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/10 13:20:55 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/10 15:41:40 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char		*ft_num2hex(long int num)
+static size_t	ft_base_depth(long int num, int base)
 {
-	char	*hex;
-	size_t	len;
+	size_t		depth;
 
-	len = 1;
-	while (num /= 2)
-		++len;
-	printf("len = %zu\n", len);
-	return (NULL);
+	depth = 1;
+	while (num /= base)
+		++depth;
+	return (depth);
+}
+
+char			*ft_num2hex_or_oct(long int num, int base)
+{
+	char		*hex;
+	size_t		len;
+	int			mod;
+
+	len = ft_base_depth(num, base);
+	if (!(hex = (char *)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	hex[len] = '\0';
+	while (len--)
+	{
+		mod = num % base;
+		hex[len] = (mod > 9 ? mod + '7' : mod + '0');
+		num /= base;
+	}
+	return (hex);
 }
