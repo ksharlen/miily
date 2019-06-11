f9537ffb03b8fd1d690682957af9e8ef3102e406
Chugtai Ormund <cormund@mi-t4.21-school.ru>
2019-06-11 17:20:37 +0300

    17:20

--- C_training/ft_printf/src/src_num_to_str/main.c
+++ C_training/ft_printf/src/src_num_to_str/main.c
@@ -6,15 +6,23 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 09:25:50 by cormund           #+#    #+#             */
-/*   Updated: 2019/06/10 15:40:35 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/11 17:19:00 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdint.h>
 
 
 int	main(int ac, char **av)
 {
-	printf("%s\n", ft_num2hex_or_oct(8, 8));
+	char *p = "a";
+	intptr_t *ptr = "a";
+
+/*	printf("%s\n", ft_num2hex_or_oct(8, 8));
+	printf("%d\n", ptr);
+	printf("%i\n", p);
+	printf("%#x\n", ptr);
+	printf("%p\n", ptr);*/
 	return (0);
 }
