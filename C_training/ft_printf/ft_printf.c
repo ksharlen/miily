a450e14ea958229f37396035f5bd2eb6d780f977
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-06-13 09:25:25 +0300

    09:25

--- C_training/ft_printf/ft_printf.c
+++ C_training/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:41 by marvin            #+#    #+#             */
-/*   Updated: 2019/06/12 21:09:05 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/13 08:18:13 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,22 @@ int				ft_printf(const char *format, ...)
 
 	g_spec.size_write = 0;
 	g_spec.paste = 0;
+	g_spec.size_buf = SIZE_BUF_PRINTF;
 	ft_bzero(buf_printf, SIZE_BUF_PRINTF);
 	va_start(form, format);
 //	while (*format)
 //	{
 	ft_work_to_format(format, buf_printf, form);
-	printf("buf = %s\n", buf_printf);
-	printf("size_write = %ld\n", g_spec.size_spec + g_spec.size_write);
-	write(1, buf_printf, g_spec.size_spec + g_spec.size_write);
+	//printf("buf = %s\n", buf_printf);
+	//printf("size_write = %ld\n", g_spec.size_spec + g_spec.size_write);
+	//write(1, buf_printf, g_spec.size_spec + g_spec.size_write);
+	g_spec.ret_printf = write(1, buf_printf, g_spec.size_write);
+	//printf("size_write = %ld\n", g_spec.size_write);
 
 		//printf("*format = %c\n", *format);
 //		format += g_spec.shift_spec;
 		//printf("format = %s\n", format);
 //		format += shift_spec;
 //	}
-	return (0);
+	return (g_spec.ret_printf);
 }
---
+++ C_training/ft_printf/src/src_work/ft_work_fwa.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_work_fwa.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/13 08:37:47 by ksharlen          #+#    #+#             */
+/*   Updated: 2019/06/13 08:41:46 by ksharlen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf"
+
+char            *ft_flags_hash(const char *)
+{
+    
+}
+
+char            *ft_flag_plus_space(const char *)
+{
+
+}
+
+char            *ft_flag_dash_zero(const char *)
+{
+    
+}
+
+char            *ft_width(const char *)
+{
+
+}
+
+char            *ft_accuracy(const char *)
+{
+    
+}
