ef6d08b4e6a6b57636b67b31486bd57ba037d44c
Chugtai Ormund <cormund@mi-t4.21-school.ru>
2019-06-14 16:24:03 +0300

    16:23

--- C_training/ft_printf/main_ft_printf.c
+++ C_training/ft_printf/main_ft_printf.c
@@ -3,22 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main_ft_printf.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2019/06/14 11:35:00 by cormund          ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2019/06/14 11:36:55 by ksharlen         ###   ########.fr       */
-=======
-/*   Updated: 2019/06/14 11:30:21 by cormund          ###   ########.fr       */
->>>>>>> 5df01c7bb81fabc97393d7bedabd84c6c4ce3011
->>>>>>> 2314810e01acc7975e2f82c59b46c10de8aaf0f0
-=======
-/*   Updated: 2019/06/14 16:04:21 by ksharlen         ###   ########.fr       */
->>>>>>> b8012d07fc7f86d97f2dc27c41413bdff4717b8f
+/*   Updated: 2019/06/14 16:22:45 by cormund          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,20 +113,9 @@ int		main(void)
 	// ft_printf("%10.3d\n", 1234);
 	// printf("%10.3d\n", 1234);
 
-<<<<<<< HEAD
-	printf("test6\n");
-<<<<<<< HEAD
-	ft_printf("%3.0d\n", 0);
-	printf("%3.0d\n", 0);
-=======
-	ft_printf("%3.10d\n", 1234);
-	printf("%3.10d\n", 1234);
-
-	printf("test7\n");
-	//printf("%d\n", 0);
-	ft_printf("%d", 0);
->>>>>>> 2314810e01acc7975e2f82c59b46c10de8aaf0f0
-=======
+	// printf("test6\n");
+	// ft_printf("%3.0d\n", 0);
+	// printf("%3.0d\n", 0);
 	// printf("test6\n");
 	// ft_printf("%3.10d\n", 1234);
 	// printf("%3.10d\n", 1234);
@@ -143,7 +123,16 @@ int		main(void)
 	// printf("test7\n");
 	//ft_printf("%d\n", 0);
 	//ft_printf("a=%10d\nb=%d\n", 25, 123);
-	ft_printf("a = %10.20d\nb = %d\n", 25, 31);
->>>>>>> b8012d07fc7f86d97f2dc27c41413bdff4717b8f
+	// ft_printf("a = %10.20d\nb = %d\n", 25, 31);
+
+	printf("test8\n");
+	ft_printf("ft_ %.1d\n", 0);
+	// ft_printf("a=%10d\nb=%d\n", 25, 123);
+	// ft_printf("a = %10.20d\nb = %d\n", 25, 31);
+	
+	printf("orig %.1d\n", 0);
+	// printf("a=%10d\nb=%d\n", 25, 123);
+	// printf("a = %10.20d\nb = %d\n", 25, 31);
+
 	return (0);
 }
