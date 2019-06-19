322ef9097e237b496d23753cf8733fbbe0391da6
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-06-19 10:49:12 +0300

    10:49

--- C_training/training/mephi/project1/ft_malloc.c
+++ C_training/training/mephi/project1/ft_malloc.c
@@ -6,15 +6,19 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 10:12:39 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/18 10:14:06 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/19 09:50:50 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void	ft_malloc(t_matrix *elem)
+void		*ft_malloc(size_t size)
 {
-	elem->elem = (t_line *)malloc(sizeof(t_line) * elem->num_list);
-	if (!elem->elem)
+	void 	*ret;
+	ret = NULL;
+
+	ret = malloc(size);
+	if (!ret)
 		ft_error(ERR_MEM);
+	return (ret);
 }
--- C_training/training/mephi/project1/main.c
+++ C_training/training/mephi/project1/main.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 00:29:56 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/18 10:18:21 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/19 10:20:14 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ int					main(void)
 {
 	t_matrix 		elem;
 
-	ft_get_int(&elem.num_list); //?Значение всегда будет правильным
-	ft_malloc(&elem);
+	printf("Введите кол-во строк: \n");
+	ft_num_line(&elem.num_list);
+	elem.elem = (t_line *)ft_malloc(sizeof(t_line) * elem.num_list);
 	ft_input_data(&elem);
+	ft_print_matrix(elem);
+	//ft_input_data(&elem);
 	//printf("elem.num = %d\n", elem.num_list);
 	return (0);
 }
