573af2b87f82599fd6e820e926046e205ddd3a80
ksharlen <miily@list.ru>
2019-06-22 21:50:33 +0300

    21:50

--- C_training/training/java/java_test/src/Main.java
+++ C_training/training/java/java_test/src/Main.java
@@ -2,19 +2,19 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		printArgs obj = new printArgs(1, 2, 3);
-		printArgs temp;
+		Stack obj = new Stack(4);
+		Stack obj2 = new Stack(6);
 
-		obj.ft_print();
-		obj.setInt(13, 24, 51);
-		obj.ft_print();
-		// System.out.printf("before method:\n");
-		// obj.ft_print();
-		// obj.setInt(4, 1, 2);
-		// System.out.println("after method");
-		// obj.ft_print();
-		// System.out.println(obj.first);
-		// System.out.println(obj.test);
-		//System.out.println(obj.a);
+		for (int i = 0; i++ < obj.size; )
+			obj.push(i);
+		for (int i = 0; i++ < obj2.size; )
+			obj2.push(i);
+
+		System.out.println("obj Stack: ");
+		for (int i = 0; i++ < obj.size; )
+			System.out.println(obj.pop());
+		System.out.println("obj1 Stack: ");
+		for (int i = 0; i++ < obj2.size; );
+			System.out.println(obj2.pop());
 	}
 }
