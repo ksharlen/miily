7446cf785f1716d77669ca95d8ec360cffc43586
Chugtai Ormund <cormund@mi-t4.21-school.ru>
2019-06-26 17:31:58 +0300

    17:31

--- C_training/ft_printf/archive/ft_int_to_str.c
+++ C_training/ft_printf/archive/ft_int_to_str.c
@@ -6,63 +6,47 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 08:18:38 by marvin            #+#    #+#             */
-/*   Updated: 2019/06/26 15:37:10 by cormund          ###   ########.fr       */
+/*   Updated: 2019/06/18 17:09:35 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static char			ft_chr_space_plus_dec()
+static size_t	ft_size_num(long long int num)
 {
-	if (g_spec.flags & DEC)
-		return ('-');
-	if (g_spec.flags & PLUS)
-		return ('+');
-	return (' ');
-}
+	size_t		l;
 
-static size_t		ft_size_num(long long int num)
-{
-	size_t			l;
-	long long int	num_cp;
-
-	num_cp = num;
+	// l = num == 0 ? 0 : 1;
 	l = g_spec.flags & DOT && g_spec.accuracy == 0 && num == 0 ? 0 : 1;
-	while (num_cp /= 10)
+	while (num /= 10)
 		++l;
 		// num /= 10;
-	g_spec.flags & DOT && g_spec.accuracy > l ? l = g_spec.accuracy : 0;
-	g_spec.flags & SPACE || g_spec.flags & PLUS || num < 0 ? ++l : 0;
-	num < 0 ? g_spec.flags ^= DEC : 0;
-	g_spec.flags & ZERO && g_spec.width > l && !(g_spec.flags & DOT) ?\
-	l = g_spec.width : 0;
 	g_spec.size_num = l;
 	return (l);
 }
 
+//g_spec.flags |= DEC;
+
 char			*ft_int_to_str(long long int num)
 {
-	char 		*str;
-	char		*str_cp;
+	char 		*all_str;
 	size_t		size_num;
 	short		sign;
-	size_t		size_str;
 
-	sign = (num < 0 ? -1 : 1);
+	sign = 1;
 	size_num = ft_size_num(num);
-	size_str = (size_num < g_spec.width ? g_spec.width : size_num);
-	str = ft_strnew(size_str);
-	if (!(str_cp = str))
+	if (!(all_str = (char *)ft_memalloc(size_num + 1)))
 		return (NULL);
-	if (g_spec.width > size_num)
-		str_cp = ft_size_work(str);
+	all_str[size_num] = '\0';
+	if (num < 0)
+	{
+		g_spec.flags |= DEC;
+		sign = -1;
+	}
 	while (size_num--)
 	{
-		str_cp[size_num] = num % 10 * sign + '0';
+		all_str[size_num] = num % 10 * sign + '0';
 		num /= 10;
 	}
-	if (g_spec.flags & SPACE || g_spec.flags & PLUS || sign == -1)
-		str_cp[0] = ft_chr_space_plus_dec();
-	g_spec.size_num = size_str;
-	return (str);
+	return (all_str);
 }
