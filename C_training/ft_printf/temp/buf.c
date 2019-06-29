16d315cf132dd0d62f465d11fd269d2f0b7c64d1
ksharlen <miily@list.ru>
2019-06-29 10:11:02 +0300

    10:10

--- C_training/ft_printf/temp/buf.c
+++ C_training/ft_printf/temp/buf.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 19:32:28 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/28 23:14:34 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/29 10:02:51 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,45 @@ static	int		ft_check_loc_buf(int size)
 		return (0);
 }
 
-void			ft_write_from_buf(const char *buf)
+void			ft_write_from_buf_and_clean(char *buf)
 {
 	if (!buf)
 		ft_work_buf(NULL, SIZE_BUF - g_buf.size_write + 1);
-	else
+	else if (g_buf.size_write > 0)
+	{
 		g_buf.ret_write += write(1, buf, g_buf.size_write);
+		ft_bzero(buf, SIZE_BUF);
+		g_buf.size_write = 0;
+		g_buf.size_buf = SIZE_BUF;
+	}
 }
 
-static void		ft_clean_buf(char *buf)
+static char		*ft_push_buf(char *inbuf, int size_inbuf, char *buf)
 {
-	ft_bzero(buf, SIZE_BUF);
-	g_buf.size_write = 0;
-	g_buf.size_buf = SIZE_BUF;
+	char		*ret_buf;
+
+	if (size_inbuf > 0 && inbuf)
+	{
+		ft_memcpy(buf + g_buf.size_write, inbuf, size_inbuf);
+		g_buf.size_buf -= size_inbuf;
+		g_buf.size_write += size_inbuf;
+		ret_buf += g_buf.size_write;
+	}
+	return (ret_buf);
 }
 
 static void		ft_write_big_data(char **inbuf, int *size_inbuf)
 {
-	while (*size_inbuf > 0)
+	while (*size_inbuf > 0 && *inbuf)
 	{
 		g_buf.ret_write += write(1, *inbuf, SIZE_BUF);
 		*size_inbuf -= SIZE_BUF;
 		*inbuf += SIZE_BUF;
-		if (*size_inbuf < SIZE_BUF)
+		if (*size_inbuf < SIZE_BUF && *size_inbuf > 0)
 		{
 			g_buf.ret_write += write(1, *inbuf, *size_inbuf);
 			*size_inbuf = 0;
 			*inbuf = NULL;
-			return ;
 		}
 	}
 }
@@ -68,22 +79,14 @@ char			*ft_work_buf(char *inbuf, int size_inbuf)
 	ret_buf = buf;
 	if (ret_check_loc == 1)
 	{
-		ft_write_from_buf(buf);
-		ft_clean_buf(buf);
+		ft_write_from_buf_and_clean(buf);
 		ret_buf = (buf + g_buf.size_write);
 	}
 	else if (ret_check_loc == -1)
 	{
-		ft_write_from_buf(buf);
-		ft_clean_buf(buf);
+		ft_write_from_buf_and_clean(buf);
 		ft_write_big_data(&p_inbuf, &size_inbuf);
 	}
-	if (size_inbuf > 0 && p_inbuf)
-	{
-		ft_memcpy(buf + g_buf.size_write, p_inbuf, size_inbuf);
-		g_buf.size_buf -= size_inbuf;
-		g_buf.size_write += size_inbuf;
-		ret_buf += g_buf.size_write;
-	}
+	ret_buf = ft_push_buf(p_inbuf, size_inbuf, buf);
 	return (ret_buf);
 }
--- C_training/ft_printf/temp/main.c
+++ C_training/ft_printf/temp/main.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 19:32:45 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/06/28 23:13:49 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/06/29 10:10:42 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,14 @@ int			main(void)
 	char *buf;
 	char *str;
 
-	str = "test str num2\n";
+	str = "hello world how are you?\nFine and you?\nMe tooo ahahahahaha\n";
 	g_buf.size_write = 0;
 	g_buf.size_buf = SIZE_BUF;
 	g_buf.ret_write = 0;
 
 	ft_work_buf(str, ft_strlen(str));
-	ft_write_from_buf(NULL);
-	printf("ret_write = %d", g_buf.ret_write);
-	//printf("buf = %s\n", buf);
-	// ft_write_from_buf(NULL);
-	// ft_work_buf(str, ft_strlen(str));
-	// ft_write_from_buf(NULL);
-	// ft_memset(buf, '0', 5);
-	// g_buf.size_write += 5;
-	// g_buf.size_buf -= 5;
-	// ft_write_from_buf(NULL);
+	ft_write_from_buf_and_clean(WRITE_BUF);
+	printf("g_buf.size_buf = %d\ng_buf.size_write = %d\n", g_buf.size_buf, g_buf.size_write);
+	//ft_write_from_buf_and_clean(WRITE_BUF);
 	return (0);
 }
