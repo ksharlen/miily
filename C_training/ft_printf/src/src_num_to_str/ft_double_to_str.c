c94c4ebdf9a4380db67bb18956e335c070d2c0fb
ildison <ildarusmanov92@gmail.com>
2019-07-03 12:44:59 +0300

    12:44

--- C_training/ft_printf/src/src_num_to_str/ft_double_to_str.c
+++ C_training/ft_printf/src/src_num_to_str/ft_double_to_str.c
@@ -6,11 +6,35 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 15:25:14 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/07/01 16:54:49 by cormund          ###   ########.fr       */
+/*   Updated: 2019/07/03 12:42:38 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+typedef struct				s_uni
+{
+	long double				num;
+	struct
+	{
+		unsigned long		mantissa:64;
+		short int			exh:15;
+		unsigned int		sign:1;
+	}						bits;
+}							t_uni;
+
+int bin_power(int t, int k) // возведение t в степень k
+{
+	int res = 1;
+	while (k)
+	{
+		if (k & 1)
+    	res *= t;
+		t *= t;
+		k >>= 1;
+    }
+  return res;
+}
 
 static long double			pull_double_arg(va_list format)
 {
@@ -24,9 +48,9 @@ static long double			pull_double_arg(va_list format)
 	return (ret);
 }
 
-void	ft_double_to_str(va_list format)
+void						ft_double_to_str(va_list format)
 {
-	long double val;
+	t_uni					val;
 
-	val = pull_double_arg(format);
+	val.num = pull_double_arg(format);
 }
