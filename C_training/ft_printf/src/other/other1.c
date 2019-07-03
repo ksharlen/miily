fdb52e226f3c9739eaf35030f1030b4f3f239396
Kharazim Sharlene <ksharlen@oa-n5.21-school.ru>
2019-07-03 10:24:06 +0300

    10:24

--- C_training/ft_printf/src/other/other1.c
+++ C_training/ft_printf/src/other/other1.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 08:58:06 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/07/03 09:15:05 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/07/03 10:17:38 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,36 +60,32 @@ int					ft_mem_rec_cmp_for_color(char *str1, char *str2, size_t n)
 	return (ft_tolower(*s1) - *s2);
 }
 
-/*
-**	void				ft_print_test(const char *form)
-**	{
-**	char			*type;
-**
-**		type = NULL;
-**		if (form)
-**			printf("string:	\"%s\"\n", form);
-**		printf("spec 	=	%c		accuracy =	%d		width		=	%d	size_num	=	%ld\n", g_spec.spec, g_spec.accuracy, g_spec.width, g_spec.size_num);
-**		printf("ZERO 	=	%d		PLUS =		%d		DASH		=	%d\n", (g_spec.flags & ZERO) && 1, (g_spec.flags & PLUS) && 1, (g_spec.flags & DASH) && 1);
-**		printf("SPACE 	=	%d		HASH =		%d		DOT		=	%d\n", g_spec.flags & SPACE && 1, g_spec.flags & HASH && 1, g_spec.flags & DOT && 1);
-**		printf("DEC 	=	%d		STAR_ACC =	%d		STAR_WITH	=	%d\n", g_spec.flags & DEC && 1, g_spec.flags & STAR_ACC && 1, g_spec.flags & STAR_WITH && 1);
-**		if (g_spec.mod & ZERO)
-**			type = "hh";
-**		else if (g_spec.mod & PLUS)
-**			type = "h";
-**		else if (g_spec.mod & DASH && ft_check_the_entry(NUM_DOUBLE, g_spec.spec))
-**			type = "L";
-**		else if (g_spec.mod & DASH)
-**			type = "l";
-**		else if (g_spec.mod & SPACE)
-**			type = "ll";
-**		else if (g_spec.mod & HASH)
-**			type = "j";
-**		else if (g_spec.mod & DOT)
-**			type = "z";
-**		else if (g_spec.mod & DEC)
-**			type = "t";
-**		printf("type 	=	%s\n", type);
-**		printf("=============================\n");
-**	}
-**
-*/
+void				ft_print_test(const char *form)
+{
+char			*type;
+	type = NULL;
+	if (form)
+		printf("string:	\"%s\"\n", form);
+	printf("spec 	=	%c		accuracy =	%d		width		=	%d	size_num	=	%ld\n", g_spec.spec, g_spec.accuracy, g_spec.width, g_spec.size_num);
+	printf("ZERO 	=	%d		PLUS =		%d		DASH		=	%d\n", (g_spec.flags & ZERO) && 1, (g_spec.flags & PLUS) && 1, (g_spec.flags & DASH) && 1);
+	printf("SPACE 	=	%d		HASH =		%d		DOT		=	%d\n", g_spec.flags & SPACE && 1, g_spec.flags & HASH && 1, g_spec.flags & DOT && 1);
+	printf("DEC 	=	%d		STAR_ACC =	%d		STAR_WITH	=	%d\n", g_spec.flags & DEC && 1, g_spec.flags & STAR_ACC && 1, g_spec.flags & STAR_WITH && 1);
+	if (g_spec.mod & ZERO)
+		type = "hh";
+	else if (g_spec.mod & PLUS)
+		type = "h";
+	else if (g_spec.mod & DASH && ft_check_the_entry(NUM_DOUBLE, g_spec.spec))
+		type = "L";
+	else if (g_spec.mod & DASH)
+		type = "l";
+	else if (g_spec.mod & SPACE)
+		type = "ll";
+	else if (g_spec.mod & HASH)
+		type = "j";
+	else if (g_spec.mod & DOT)
+		type = "z";
+	else if (g_spec.mod & DEC)
+		type = "t";
+	printf("type 	=	%s\n", type);
+	printf("=============================\n");
+}
