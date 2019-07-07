2987f029df2c7bd6ea85cd9d9d9aff5e42146c7e
ksharlen <miily@list.ru>
2019-07-07 10:29:04 +0300

    testing commit

---
+++ C_training/training/java/projects/project3/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * java My first program
+ *
+ * @author ksharlen
+ * @version dated
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); //output
+    }
+}
