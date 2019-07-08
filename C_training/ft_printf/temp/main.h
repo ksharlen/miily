de4ff664ed56e032563e30e472b89dec2dde0dc7
Kharazim Sharlene <ksharlen@oa-n5.21-school.ru>
2019-07-08 17:12:20 +0300

    17:12

--- C_training/ft_printf/temp/main.h
+++ C_training/ft_printf/temp/main.h
@@ -15,6 +15,23 @@ typedef struct	s_data
 	int			years;
 }				t_data;
 
+typedef enum	e_mounth
+{
+	JAN = 31,
+	MAR = 31,
+	APR = 30,
+	MAY = 31,
+	JUN = 30,
+	JUL = 31,
+	AUG = 31,
+	SEP = 30,
+	OCT = 31,
+	NOV = 30,
+	DECEMBER = 31,
+}				t_mounth;
+
 void	data_to_str(unsigned long long int num_data);
+int		get_quan_days(int month);
+int		get_leap(int years);
 
 #endif
