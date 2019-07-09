eb7a514ba0e111b04a3093f0f5499bf3983ab155
Kharazim Sharlene <ksharlen@oa-n5.21-school.ru>
2019-07-09 09:55:10 +0300

    09:55

--- C_training/ft_printf/temp/main.c
+++ C_training/ft_printf/temp/main.c
@@ -3,23 +3,17 @@
 
 t_spec	g_spec;
 
+
+
 int		main(void)
 {
 	char *testing;
-
-/*
-**	data_to_str(va_list format, unsigned long long int data)
-**	{
-**		//вызов вытаскивания аргумента
-**		//вызов уплотнения data в структуру
-**		//вызов обработки переполнения
-**		//вызов ф-ии для добавления результата в буфер
-**	}
-*/
-	g_spec.size_write = 0;
-	g_spec.size_buf = SIZE_BUF;
-	data_to_str(0);
-
+	size_t	len;
+	// g_spec.size_write = 0;
+	// g_spec.size_buf = SIZE_BUF;
+	// data_to_str(4846846846848987987);
+	len = ft_size_num(124123);
+	printf("len = %ld\n", len);
 	return (0);
 }
 
