c92e281c1ba1fe822fb888b15e97e6e89a1d9099
ksharlen <miily@list.ru>
2019-07-13 12:53:29 +0300

    commit before refactor

--- C_training/ft_printf/main_ft_printf.c
+++ C_training/ft_printf/main_ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_ft_printf.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-/*   Updated: 2019/07/12 18:14:14 by cormund          ###   ########.fr       */
+/*   Updated: 2019/07/13 12:40:50 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,18 @@ int		main(void)
 	// test_f();
 	// test_other();
 
-	int ret_ft;
-	int ret;
+	// int ret_ft;
+	// int ret;
 
-	ft_printf("|%.6e|\n", 90.50);
-	printf("|%.0f|\n", 90.50);
+//	ft_printf("|%.6e|\n", 90.50);
+//	printf("|%.0f|\n", 90.50);
 	// printf("|%.0e|\n", 0.0000000000000099);
 	// ret_ft = ft_printf("%6.10d\n", 1234);
 	// ret = printf("%6.10d\n", 1234);
 	// printf("ret_ft = %d\nret = %d\n", ret_ft, ret);
 	// ft_printf("time: \n%k\n%k\n%s", 199203010325, 9506012200, "hello world\n");
+
+//ft_printf("%f\n", 13.0);
 
 	return (0);
 }
