3fe7a76999ac548b190ddbea76e064f8ad54b4cb
Chugtai Ormund <cormund@oa-o2.21-school.ru>
2019-07-15 10:31:05 +0300

    befor merge

--- C_training/ft_printf/main_test/main_ft_printf.c
+++ C_training/ft_printf/main_test/main_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 07:55:45 by marvin            #+#    #+#             */
-/*   Updated: 2019/07/15 09:39:57 by cormund          ###   ########.fr       */
+/*   Updated: 2019/07/15 10:16:18 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,25 @@
 
 int		main(void)
 {
-	test_s();
-	test_o();
-	test_x();
-	test_d();
-	test_f();
-	test_other();
+	// test_s();
+	// test_o();
+	// test_x();
+	// test_d();
+	// test_f();
+	// test_other();
 
 	// int ret_ft;
 	// int ret;
 
-//	ft_printf("|%.6e|\n", 90.50);
-//	printf("|%.0f|\n", 90.50);
+	ft_printf("|%.1f|\n", 9.550);
+	printf("|%.1f|\n", 9.550);
 	// printf("|%.0e|\n", 0.0000000000000099);
 	// ret_ft = ft_printf("%6.10d\n", 1234);
 	// ret = printf("%6.10d\n", 1234);
 	// printf("ret_ft = %d\nret = %d\n", ret_ft, ret);
 	// ft_printf("time: \n%k\n%k\n%s", 199203010325, 9506012200, "hello world\n");
 
-ft_printf("%f\n", 13.0);
+	// printf("%f\n", 125.00);
 
 	return (0);
 }
