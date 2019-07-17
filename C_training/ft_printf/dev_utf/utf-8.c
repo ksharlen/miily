308d0529924e1142950b5f1cd799fae1d334e6ee
ksharlen <miily@list.ru>
2019-07-17 21:06:25 +0300

    success maked utf-8, need push to buf this

--- C_training/ft_printf/dev_utf/utf-8.c
+++ C_training/ft_printf/dev_utf/utf-8.c
@@ -1,60 +1,24 @@
 #include "main.h"
 
-unsigned set_bit(unsigned val, int num)
+static t_utf push_unicode(t_utf utf)
 {
-	return (val | (1 << num));
-}
+	int index;
 
-unsigned set_bits(unsigned val, unsigned bits, int num)
-{
-	while (bits--)
-		val = set_bit(val, num--);
-	return (val);
-}
-
-static	unsigned set_bits_in(unsigned value, unsigned inval, int num)
-{
-	return (value | (inval << num));
-}
-
-static unsigned set_mask_10(unsigned val, int bytes)
-{
-	--bytes;
-	while (bytes)
+	index = 0;
+	while (utf.bytes)
 	{
-		val = set_bit(val, (bytes * 8) - 1);
-		bytes--;
+		utf.utf_sym = set_bits_in(utf.utf_sym, get_6_bits(utf.unicode), index);
+		utf.unicode >>= 6;
+		index += 8;
+		--utf.bytes;
 	}
-	return (val);
+	return (utf);
 }
 
-int		def_num_bytes(wchar_t unicode)
+t_utf work_utf(t_utf utf)
 {
-	int	bytes;
-
-	bytes = -1;
-	if (unicode >= 0 && unicode < 128)
-		bytes = 1;
-	else if (unicode >= 128 && unicode < 2048)
-		bytes = 2;
-	else if (unicode >= 2048 && unicode < 65536)
-		bytes = 3;
-	else if (unicode >= 65536 && unicode < 1114112)
-		bytes = 4;
-	return (bytes);
-}
-
-t_utf	inst_mask(t_utf utf)
-{
-	int mask;
-
-	mask = 0;
-	utf.utf_sym = 0;
-	if (utf.bytes > 1)
-	{
-		mask = set_bits(mask, utf.bytes, utf.bytes - 1);
-		utf.utf_sym = set_bits_in(utf.utf_sym, mask, (utf.bytes * 8) - utf.bytes);
-		utf.utf_sym = set_mask_10(utf.utf_sym, utf.bytes);
-	}
+	//Проверить на кол-во байт, если 1 то ничего делать не надо
+	utf = inst_mask(utf);
+	utf = push_unicode(utf);
 	return (utf);
 }
