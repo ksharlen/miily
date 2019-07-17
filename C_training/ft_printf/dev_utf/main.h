d6601591a62f8e9aec7070c2e71990f6f875441c
ksharlen <miily@list.ru>
2019-07-17 14:55:11 +0300

    add mask for utf-8

--- C_training/ft_printf/dev_utf/main.h
+++ C_training/ft_printf/dev_utf/main.h
@@ -4,4 +4,17 @@
 # include <wchar.h>
 # include <stdio.h>
 
+
+typedef struct 	s_utf
+{
+	unsigned	unicode;
+	int			bytes;
+	unsigned	utf_sym;
+}				t_utf;
+
+int			def_num_bytes(wchar_t unicode);
+t_utf		inst_mask(t_utf utf);
+unsigned	set_bit(unsigned val, int num);
+unsigned	set_bits(unsigned val, unsigned bits, int num);
+
 #endif
