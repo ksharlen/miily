2894e3c40cf781d25cc1a0a618e9cca760f46ca9
ksharlen <miily@list.ru>
2019-07-17 21:06:25 +0300

    success maked utf-8, need push to buf this

--- C_training/ft_printf/dev_utf/main.h
+++ C_training/ft_printf/dev_utf/main.h
@@ -7,14 +7,18 @@
 
 typedef struct 	s_utf
 {
-	unsigned	unicode;
+	wchar_t		unicode;
 	int			bytes;
 	unsigned	utf_sym;
 }				t_utf;
 
-int			def_num_bytes(wchar_t unicode);
-t_utf		inst_mask(t_utf utf);
-unsigned	set_bit(unsigned val, int num);
-unsigned	set_bits(unsigned val, unsigned bits, int num);
+int				def_num_bytes(wchar_t unicode);
+t_utf			inst_mask(t_utf utf);
+unsigned		set_bit(unsigned val, int num);
+unsigned		set_bits(unsigned val, unsigned bits, int num);
+unsigned 		set_bits_in(unsigned value, unsigned inval, int num);
+unsigned		bitcpy(unsigned dest, unsigned from, int num, int pos);
+unsigned char	get_6_bits(unsigned unicode);
+t_utf			work_utf(t_utf utf);
 
 #endif
---
+++ C_training/ft_printf/dev_utf/mask.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+unsigned set_bit(unsigned val, int num)
+{
+	return (val | (1 << num));
+}
+
+unsigned set_bits(unsigned val, unsigned bits, int num)
+{
+	while (bits--)
+		val = set_bit(val, num--);
+	return (val);
+}
+
+unsigned set_bits_in(unsigned value, unsigned inval, int num)
+{
+	/*num - позиция с которой начнется заполнение в старшую сторону */
+	return (value | (inval << num));
+}
+
+static unsigned set_mask_10(unsigned val, int bytes)
+{
+	--bytes;
+	while (bytes)
+	{
+		val = set_bit(val, (bytes * 8) - 1);
+		bytes--;
+	}
+	return (val);
+}
+
+unsigned char get_6_bits(unsigned unicode)
+{
+	unsigned char buf;
+
+	buf = unicode;
+	buf <<= 2;
+	buf >>= 2;
+	return (buf);
+}
+
+int		def_num_bytes(wchar_t unicode)
+{
+	int	bytes;
+
+	bytes = -1;
+	if (unicode >= 0 && unicode < 128)
+		bytes = 1;
+	else if (unicode >= 128 && unicode < 2048)
+		bytes = 2;
+	else if (unicode >= 2048 && unicode < 65536)
+		bytes = 3;
+	else if (unicode >= 65536 && unicode < 1114112)
+		bytes = 4;
+	return (bytes);
+}
+
+t_utf	inst_mask(t_utf utf)
+{
+	int mask;
+
+	mask = 0;
+	utf.utf_sym = 0;
+	if (utf.bytes > 1)
+	{
+		mask = set_bits(mask, utf.bytes, utf.bytes - 1);
+		utf.utf_sym = set_bits_in(utf.utf_sym, mask, (utf.bytes * 8) - utf.bytes);
+		utf.utf_sym = set_mask_10(utf.utf_sym, utf.bytes);
+	}
+	return (utf);
+}
