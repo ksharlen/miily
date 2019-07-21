c6e80aab1d1cf719c1e6b6de6be9e263d3aaad99
ksharlen <miily@list.ru>
2019-07-21 16:16:33 +0300

    add folder test

---
+++ C_training/ft_printf/include/testing.h
@@ -0,0 +1,13 @@
+#ifndef TESTING_H
+# define TESTING_H
+
+# include "ft_printf.h"
+
+void	test_d(void);
+void	test_o(void);
+void	test_c(void);
+void	test_x(void);
+void	test_s(void);
+void	test_other(void);
+
+#endif
